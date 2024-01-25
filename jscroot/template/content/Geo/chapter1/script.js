3e59688cfd066edd409118d97f384b3cfc90cf91
“MSyahidAlFajri” <masyahida4@gmail.com>
2024-01-26 04:32:53 +0700

    update

--- jscroot/template/content/Geo/chapter1/script.js
+++ jscroot/template/content/Geo/chapter1/script.js
@@ -24,7 +24,7 @@ export function main() {
     const polygonTable = document.getElementById("polygonTable").getElementsByTagName('tbody')[0];
     const polylineTable = document.getElementById("polylineTable").getElementsByTagName('tbody')[0];
 
-    fetch("./Aceh.json")
+    fetch("https://raw.githubusercontent.com/gis5syahid/skeleton/main/jscroot/template/content/Geo/chapter1/Aceh.json")
         .then(response => response.json())
         .then(data => {
             data.features.forEach(feature => {
