382902cc6afec4b1ccf587b0ff7ef6a7bcf9dcab
“MSyahidAlFajri” <masyahida4@gmail.com>
2024-02-02 06:18:01 +0700

    update

--- jscroot/template/content/Geo/chapter3/js/config/peta.js
+++ jscroot/template/content/Geo/chapter3/js/config/peta.js
@@ -9,7 +9,7 @@ import {container} from 'https://jscroot.github.io/element/croot.js';
 
 const attributions = '<a href="https://petapedia.github.io/" target="_blank">&copy; PetaPedia Indonesia</a> ';
 
-const place = [103.81128613262871,1.3247366000091603];
+const place = [95.31678153983819, 5.554925722443841];
 
 export let idmarker = {id:1};
 
