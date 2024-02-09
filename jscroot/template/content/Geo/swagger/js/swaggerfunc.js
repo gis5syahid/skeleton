922a20e968af10faa424fd17406dc945fcf1f0df
“MSyahidAlFajri” <masyahida4@gmail.com>
2024-02-09 18:38:33 +0700

    update

--- jscroot/template/content/Geo/swagger/js/swaggerfunc.js
+++ jscroot/template/content/Geo/swagger/js/swaggerfunc.js
@@ -1,6 +1,6 @@
 import SwaggerUIBundle from 'https://cdn.skypack.dev/swagger-ui-dist/swagger-ui-bundle.js';
 
-export let URLData = "https://raw.githubusercontent.com/gis5syahid/swagger/main/yaml/openAPI.yaml";//url data yaml
+export let URLData = "https://raw.githubusercontent.com/gis5syahid/skeleton/main/jscroot/template/content/Geo/swagger/yaml/openAPI.yaml";//url data yaml
 
 export const UIData = SwaggerUIBundle({
     url: URLData, //Location of Open API spec in the repo
